d3680cd04c1b6b9ab0bf1d77580af216c4dc604b
Frederico02 <81171707+Frederico02@users.noreply.github.com>
2023-03-20 23:22:29 -0300

    Update ATIVIDADE03.java

--- ATIVIDADE03.java
+++ ATIVIDADE03.java
@@ -1,1 +1,50 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
 
+public class Main {
+    public static void main(String[] args) {
+        // Passo 1
+        ArrayList<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        lista.add(5);
+
+        // Passo 2
+        Stack<Integer> pilha = new Stack<>();
+        while (!lista.isEmpty()) {
+            pilha.push(lista.remove(0));
+        }
+
+        // Passo 3
+        LinkedList<Integer> fila = new LinkedList<>();
+        while (!pilha.empty()) {
+            fila.offer(pilha.pop());
+        }
+
+        // Passo 4
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(9);
+        lista.add(10);
+
+        // Passo 5
+        pilha.clear();
+        while (!lista.isEmpty()) {
+            pilha.push(lista.remove(0));
+        }
+
+        LinkedList<Integer> fila2 = new LinkedList<>();
+        while (!pilha.empty()) {
+            fila2.offer(pilha.pop());
+        }
+
+        fila.addAll(fila2);
+
+        // Passo 6
+        System.out.println(fila);
+    }
+}
